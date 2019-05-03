61b27ba33a3f0d554b40b665e22e9c4d58e3f191
dvilajeti01 <dvilajeti01@manhattan.edu>
2019-05-03 09:47:42 -0400

    Update Sudoku.h

--- Sudoku.h
+++ Sudoku.h
@@ -2,8 +2,6 @@
 //#define 9 9
 
 #include <iostream>
-#include <queue>
-#include <utility>
 using namespace std;
 
 class Sudoku
@@ -15,19 +13,27 @@ private:
 	
 public:
 
+    //Constructor which accepts a board configuration as a parameter
 	Sudoku(int initial[9][9]);
 
+    //Calls the Backtracking function
 	void solve();
 
+    //Implementation of the Backtracking Algorithm
 	bool Backtracking(int board[9][9]);
 
+    //copies the contents of one array to the other
 	void copyArray(int to_array[9][9], int fromArray[9][9]);
 
+    //Checks for constraint satisfaction
 	bool alldiff(int value, int row, int col);
 
+    //checks if there are any un-completed cells in the board
 	bool isComplete();
 
+    //finds the next un-assigned cell in the board
 	void findVariables(int *row, int *col);
 
+    //prints the board
 	void printBoard(int board[9][9]);
 };
